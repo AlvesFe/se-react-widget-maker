aaa6fc8f3ec9391a64034f69996e0dcfb62e3b48
Felipe Batista Alves <lipe.batistao@live.com>
2025-03-23 14:07:18 -0300

    feat: update build process to output the files in a simplier way, ready to use by the streamelements overlay editor

---
+++ scripts/build.ts
@@ -0,0 +1,34 @@
+import { execSync } from 'child_process';
+import { copyFileSync, existsSync, mkdirSync, readdirSync, rmSync } from 'fs';
+import { join } from 'path';
+import chalk from 'chalk';
+
+const root = process.cwd();
+const dist = join(root, 'dist');
+const build = join(root, 'build');
+
+execSync('yarn tsc -b && yarn vite build', { stdio: 'inherit' });
+
+if (!existsSync(build)) {
+  mkdirSync(build);
+} else {
+  rmSync(build, { recursive: true });
+  mkdirSync(build);
+}
+
+console.log(chalk.white('Copying files to the build folder...'));
+copyFileSync(join(dist, 'index.html'), join(build, 'index.html'));
+readdirSync(join(dist, 'assets')).forEach(file => {
+  if (file.endsWith('.js')) {
+    copyFileSync(join(dist, 'assets', file), join(build, 'index.js'));
+  }
+});
+
+rmSync(dist, { recursive: true });
+
+console.log(chalk.green('Files copied to the build folder!'));
+
+console.table([
+  { 'File': 'HTML', 'Path': 'build/index.html' },
+  { 'File': 'JS', 'Path': 'build/index.js' },
+]);
