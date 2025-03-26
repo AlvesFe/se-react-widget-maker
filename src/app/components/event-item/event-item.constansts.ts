10edba948b516549f78c38592dae7750961c70e2
Felipe Batista Alves <lipe.batistao@live.com>
2025-03-25 21:28:55 -0300

    feat: update EventTypeStateMap to use StreamEventType constants

--- src/app/components/event-item/event-item.constansts.ts
+++ src/app/components/event-item/event-item.constansts.ts
@@ -2,12 +2,12 @@ import { StreamEventType } from '../../../providers/stream-provider/types/stream
 import { EventTypeState } from './event-item.types';
 
 export const EventTypeStateMap: ReadonlyMap<StreamEventType, EventTypeState> = new Map([
-  ['message' , { bgColor: 'rgb(146, 246, 243)', textColor: 'rgb(0, 0, 0)' }],
-  ['follow', { bgColor: 'rgb(255, 39, 39)', textColor: 'rgb(255, 255, 255)' }],
-  ['follower-latest', { bgColor: 'rgb(255, 39, 39)', textColor: 'rgb(255, 255, 255)' }],
-  ['subscriber', { bgColor: 'rgb(88, 252, 0)', textColor: 'rgb(0, 0, 0)' }],
-  ['cheer' , { bgColor: 'rgb(255, 255, 0)', textColor: 'rgb(0, 0, 0)' }],
-  ['raid', { bgColor: 'rgb(0, 34, 255)', textColor: 'rgb(255, 255, 255)' }],
+  [StreamEventType.SUBSCRIBER, { bgColor: 'rgb(146, 246, 243)', textColor: 'rgb(0, 0, 0)' }],
+  [StreamEventType.FOLLOWER_LATEST, { bgColor: 'rgb(255, 39, 39)', textColor: 'rgb(255, 255, 255)' }],
+  [StreamEventType.FOLLOW, { bgColor: 'rgb(255, 39, 39)', textColor: 'rgb(255, 255, 255)' }],
+  [StreamEventType.CHEER, { bgColor: 'rgb(88, 252, 0)', textColor: 'rgb(0, 0, 0)' }],
+  [StreamEventType.RAID, { bgColor: 'rgb(255, 255, 0)', textColor: 'rgb(0, 0, 0)' }],
+  [StreamEventType.MESSAGE, { bgColor: 'rgb(0, 34, 255)', textColor: 'rgb(255, 255, 255)' }],
 ]);
 
 export const DefaultState: EventTypeState = { bgColor: 'rgb(255, 255, 255)', textColor: 'rgb(0, 0, 0)' };
